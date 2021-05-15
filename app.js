775be10103d229feadbfce558697987eeeae0eee
Federico Barbieri <federicobarbieri54@gmail.com>
2021-05-15 12:26:23 +0200

    italy, ireland, nigeria

--- app.js
+++ app.js
@@ -41,6 +41,27 @@ function randomNum() {
   const coteWhite = document.querySelector('.cote-white');
   const coteGreen = document.querySelector('.cote-green');
 
+// Italy
+
+  const italy = document.querySelector('.italy');
+  const italyGreen = document.querySelector('.italy-green');
+  const italyWhite = document.querySelector('.italy-white');
+  const italyRed = document.querySelector('.italy-red');
+
+  // Ireland
+
+    const ireland = document.querySelector('.ireland');
+    const irelandGreen = document.querySelector('.ireland-green');
+    const irelandWhite = document.querySelector('.ireland-white');
+    const irelandOrange = document.querySelector('.ireland-orange');
+
+  // Ireland
+
+    const nigeria = document.querySelector('.nigeria');
+    const nigeriaGreen = document.querySelector('.nigeria-green');
+    const nigeriaWhite = document.querySelector('.nigeria-white');
+    const nigeriaGreen2 = document.querySelector('.nigeria-green2');
+
 
 // counter for every time a country is selected
 
@@ -90,6 +111,42 @@ if (selectCountry.value === '3') {
 
 }
 
+// if Italy is selected - opacity
+
+if (selectCountry.value === '4') {
+  counter +=1;
+  italy.style.opacity = "1";
+  italy.style.zIndex = counter;
+  italyGreen.style.backgroundColor = "rgb(0, 140, 69)";
+  italyWhite.style.backgroundColor = "rgb(244, 245, 240)";
+  italyRed.style.backgroundColor = "rgb(205, 33, 42)";
+
+}
+
+// if Ireland is selected - opacity
+
+if (selectCountry.value === '5') {
+  counter +=1;
+  ireland.style.opacity = "1";
+  ireland.style.zIndex = counter;
+  irelandGreen.style.backgroundColor = "rgb(22,155,98)";
+  irelandWhite.style.backgroundColor = "rgb(255,255,255)";
+  irelandOrange.style.backgroundColor = "rgb(255, 130, 0)";
+
+}
+
+// if nigeria is selected - opacity
+
+if (selectCountry.value === '6') {
+  counter +=1;
+  nigeria.style.opacity = "1";
+  nigeria.style.zIndex = counter;
+  nigeriaGreen.style.backgroundColor = "rgb(0, 150, 57)";
+  nigeriaWhite.style.backgroundColor = "rgb(255,255,255)";
+  nigeriaGreen2.style.backgroundColor = "rgb(0, 150, 57)";
+
+}
+
 })
 
 
@@ -122,6 +179,33 @@ changeColorBtn.addEventListener('click', () => {
     coteOrange.style.backgroundColor = 'rgb(' + randomNum() + ',' + randomNum() + ',' + randomNum() + ')';
     coteWhite.style.backgroundColor = 'rgb(' + randomNum() + ',' + randomNum() + ',' + randomNum() + ')';
     coteGreen.style.backgroundColor = 'rgb(' + randomNum() + ',' + randomNum() + ',' + randomNum() + ')';
+  }
+
+  // if Italy is selected - change color
+
+  if (selectCountry.value === '4') {
+
+    italyGreen.style.backgroundColor = 'rgb(' + randomNum() + ',' + randomNum() + ',' + randomNum() + ')';
+    italyWhite.style.backgroundColor = 'rgb(' + randomNum() + ',' + randomNum() + ',' + randomNum() + ')';
+    italyRed.style.backgroundColor = 'rgb(' + randomNum() + ',' + randomNum() + ',' + randomNum() + ')';
+  }
+
+  // if Ireland is selected - change color
+
+  if (selectCountry.value === '5') {
+
+    irelandGreen.style.backgroundColor = 'rgb(' + randomNum() + ',' + randomNum() + ',' + randomNum() + ')';
+    irelandWhite.style.backgroundColor = 'rgb(' + randomNum() + ',' + randomNum() + ',' + randomNum() + ')';
+    irelandOrange.style.backgroundColor = 'rgb(' + randomNum() + ',' + randomNum() + ',' + randomNum() + ')';
+  }
+
+  // if Nigeria is selected - change color
+
+  if (selectCountry.value === '6') {
+
+    nigeriaGreen.style.backgroundColor = 'rgb(' + randomNum() + ',' + randomNum() + ',' + randomNum() + ')';
+    nigeriaWhite.style.backgroundColor = 'rgb(' + randomNum() + ',' + randomNum() + ',' + randomNum() + ')';
+    nigeriaGreen2.style.backgroundColor = 'rgb(' + randomNum() + ',' + randomNum() + ',' + randomNum() + ')';
   }
 
 
